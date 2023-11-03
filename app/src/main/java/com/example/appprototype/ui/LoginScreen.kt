6630f46ef7da95fa86b8dbee7c316a9f8ed460ab
Keane Zhang <wristy8@gmail.com>
2023-11-03 19:41:24 -0400

    password

--- app/src/main/java/com/example/appprototype/ui/LoginScreen.kt
+++ app/src/main/java/com/example/appprototype/ui/LoginScreen.kt
@@ -4,14 +4,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.Visibility
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.appprototype.viewmodels.MainViewModel
 import com.example.appprototype.viewmodels.Screen
@@ -19,21 +31,39 @@ import com.example.appprototype.viewmodels.Screen
 @Composable
 @Preview
 fun LoginScreen(mainViewModel: MainViewModel = viewModel()) {
+
+    var password by rememberSaveable { mutableStateOf("") }
+    var passwordHidden by rememberSaveable { mutableStateOf(true) }
+    var text by rememberSaveable { mutableStateOf("") }
+
     Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         // Username field, password field, etc.
 
         TextField(
-            value = "my@mail.com", // TODO:
-            onValueChange = { it },
+            value = text, // TODO:
+            onValueChange = { text = it },
             label = { Text("Email") },
         )
 
         Spacer(modifier = Modifier.height(8.dp))
 
         TextField(
-            value = "*****",
-            onValueChange = { it },
+            value = password,
+            onValueChange = {  password = it  },
             label = { Text("Password") },
+            singleLine = true,
+            visualTransformation =
+            if (passwordHidden) PasswordVisualTransformation() else VisualTransformation.None,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+//            trailingIcon = {
+//                IconButton(onClick = { passwordHidden = !passwordHidden }) {
+//                    val visibilityIcon =
+//                        if (passwordHidden) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+//                    // Please provide localized description for accessibility services
+//                    val description = if (passwordHidden) "Show password" else "Hide password"
+//                    Icon(imageVector = visibilityIcon, contentDescription = description)
+//                }
+//            }
         )
 
         Button(onClick = { mainViewModel.navigateTo(Screen.Registration) }) {
